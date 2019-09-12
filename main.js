d755505896d68f63f578539a6fdd758db21d78dc
VMaksimovX073786 <Vitalii_Maksimov@contractor.amat.com>
2019-09-12 18:57:39 +0300

    improve logic

--- main.js
+++ main.js
@@ -1,83 +1,177 @@
-var zero = "0";
-var point = ".";
+var constants = {
+    point: ".",    
+};
 
 var app = new Vue({
         el: '#app',
         data: {
-            _first: zero,
-            _operation: null,
-            value: zero,
+            needToClearEntry: false,
+            needToSaveFirst: false,
+            first: null,
+            second: null,
+            operation: null,
+            value: "0",
         },        
-        methods: {
-            onDigitClicked: function(event) {
-                var digit = event.srcElement.value;
+        computed: {
+            canBackspace: function() {
+                return (
+                    (this.value !== "0") && 
+                    !this.needToClearEntry
+                );
+            },
 
-                if (this.value === zero) {
+            canAppendDigit: function() {
+                return (
+                    (this.value.length <= 15) || 
+                    this.needToClearEntry
+                );
+            },
+
+            canAppendZero: function() {
+                return this.value !== "0";
+            },
+
+            canAppendPoint: function() {
+                return !this.value.includes(constants.point);
+            },
+
+            canCalculateSquareRoot: function() {
+                return parseFloat(this.value) >= 0;
+            },            
+
+            canInverseAdditive: function() {
+                return this.value !== "0";
+            },
+
+            canInverseMultiplicative: function() {
+                return parseFloat(this.value) !== 0;
+            }
+        },
+        methods: {
+            backspace: function() {                
+                this.value = this.value.substring(0, this.value.length - 1);
+
+                if ((this.value === "") ||
+                    (this.value === "-") ||
+                    (this.value === "-0")) {                
+                    this.value = "0";
+                }
+            },
+
+            clear: function() {
+                this.needToClearEntry = false;
+                this.needToSaveFirst = false;
+                this.first = null;
+                this.second = null;
+                this.operation = null;
+                this.value = "0";
+            },
+
+            clearEntry: function() {
+                this.value = "0";
+            },
+
+            calculate: function() {
+                if (this.first == null) {
+                    this.first = this.value;
+                }
+
+                if (this.second == null) {
+                    this.second = this.value;
+                }
+
+                var value = this.value;
+                var first = parseFloat(this.first);
+                var second = parseFloat(this.second);                
+
+                switch (this.operation) {
+                    case "+":
+                        value = first + second;
+                        break;
+                    case "-":
+                        value = first - second;
+                        break;
+                    case "*":
+                        value = first * second;
+                        break;
+                    case "/":
+                        value = first / second;
+                        break;
+                }
+
+                this.value = value.toString();
+                this.first = null;                
+                this.needToClearEntry = true;
+                this.needToSaveFirst = false;
+            },
+
+            appendOperation: function(event) {
+                if (this.first != null) {
+                    this.calculate();
+                }
+                
+                this.second = null;
+                this.operation = event.srcElement.value;
+                this.needToClearEntry = true;
+                this.needToSaveFirst = true;
+            },
+
+            appendDigit: function(event) {
+                if (this.needToSaveFirst) {
+                    this.first = this.value;
+                    this.needToSaveFirst = false;
+                }
+
+                if (this.needToClearEntry) {
+                    this.clearEntry();
+                    this.needToClearEntry = false;
+                }
+
+                var digit = event.srcElement.value;                
+
+                if (this.value === "0") {
                     this.value = digit;
                 } else {
                     this.value += digit;
                 }
             },
 
-            onPointClicked: function() {
-                if (!this.value.includes(point)) {
-                    this.value += point;
+            appendPoint: function() {
+                this.value += constants.point;
+            },
+
+            calculateSquareRoot: function() {
+                this.makeUnaryValueOperation(value => Math.sqrt(value));
+            },            
+
+            inverseAdditive: function() {
+                this.makeUnaryTextOperation(text => {
+                    if (text.startsWith("-")) {
+                        return text.substring(1);
+                    } else {
+                        return "-" + text;
+                    }
+                });
+            },
+
+            inverseMultiplicative: function() {
+                this.makeUnaryValueOperation(value => 1 / value);
+            },
+
+            makeUnaryTextOperation: function(unaryOperation) {
+                if (this.needToSaveFirst) {
+                    this.first = this.value;
+                    this.needToSaveFirst = false;
                 }
+
+                this.value = unaryOperation(this.value);
+
+                this.needToClearEntry = true;
+                this.needToSaveFirst = false;
             },
 
-            onSqrtClicked: function() {
-                this.value = Math.sqrt(this.value);
-            },
-
-            onBackspaceClicked: function() {
-                if (this.value.length > 1) {
-                    this.value = this.value.substring(0, this.value.length - 1);
-                } else {
-                    this.value = zero;
-                }
-            },
-
-            onAdditiveInverseClicked: function() {
-                if (this.value === zero) {
-                    return;
-                }
-
-                if (this.value.startsWith("-")) {
-                    this.value = this.value.substring(1);
-                } else {
-                    this.value = "-" + this.value;
-                }
-
-            },
-
-            onMultiplicativeInverseClicked: function() {
-                this.value = 1 / this.value;
-            },
-
-            onClearClicked: function() {
-                this.value = zero;
-            },
-
-            onOperationClicked: function(event) {
-                this._first = this.value;
-                this._operation = event.srcElement.value;                
-            },
-
-            onCalculateClicked: function() {
-                switch (this._operation) {
-                    case "+":
-                        this.value = parseFloat(this._first) + parseFloat(this.value);
-                        break;
-                    case "-":
-                        this.value = parseFloat(this._first) - parseFloat(this.value);
-                        break;
-                    case "*":
-                        this.value = parseFloat(this._first) * parseFloat(this.value);
-                        break;
-                    case "/":
-                        this.value = parseFloat(this._first) / parseFloat(this.value);
-                        break;
-                }
+            makeUnaryValueOperation: function(unaryOperation) {
+                this.makeUnaryTextOperation(text => unaryOperation(parseFloat(text)).toString());
             }
         }
     });
